3cae12f17d7e1bce523d22fba6cd6614c5a875be
orlando <jiax.xie@foxmail.com>
2025-02-22 23:57:30 -0600

    密码加密

--- go.mod
+++ go.mod
@@ -1,11 +1,19 @@
 module shop
 
-go 1.23
+go 1.23.0
 
-require gorm.io/gorm v1.25.12
+toolchain go1.23.2
 
 require (
+	github.com/anaskhan96/go-password-encoder v0.0.0-20201010210601-c765b799fd72
+	gorm.io/driver/mysql v1.5.7
+	gorm.io/gorm v1.25.12
+)
+
+require (
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	golang.org/x/text v0.16.0 // indirect
+	golang.org/x/crypto v0.34.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
 )
---
+++ user_srv/model/main/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"crypto/md5"
+	"crypto/sha512"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"time"
+
+	"github.com/anaskhan96/go-password-encoder"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
+	//"shop/user_srv/model"
+)
+
+func genMd5(code string) string {
+	Md5 := md5.New()
+	_, _ = io.WriteString(Md5, code)
+	return hex.EncodeToString(Md5.Sum(nil))
+}
+
+func main() {
+	dsn := "root:root@tcp(192.168.61.33:3306)/shop_user_srv?charset=utf8mb4&parseTime=True&loc=Local"
+
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold: time.Second, // 慢 SQL 阈值
+			LogLevel:      logger.Info, // Log level
+			Colorful:      true,        // 彩色打印
+		},
+	)
+
+	// 全局模式
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true,
+		},
+		Logger: newLogger,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	//_ = db.AutoMigrate(&model.User{})
+
+	options := &password.Options{16, 100, 32, sha512.New}
+	salt, encodedPwd := password.Encode("admin123", options)
+	newPassword := fmt.Sprintf("$pbkdf2-sha512$%s$%s", salt, encodedPwd) //拼接加密后的密码
+	fmt.Println(newPassword)
+
+	//passwordInfo := strings.Split(newPassword, "$")
+	//fmt.Println(passwordInfo)
+	//check := password.Verify("generic password", passwordInfo[2], passwordInfo[3], options)
+	//fmt.Println(check) // true
+
+}
